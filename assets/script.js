8ca607220006b9155896d0edc999fb5b23d9efb9
Joseph Blair <adamblair176@gmail.com>
2023-08-13 00:18:50 -0700

    js changes

--- assets/script.js
+++ assets/script.js
@@ -29,7 +29,7 @@ selectTopic();
 
 /*
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-var randomTopic = topics[Math, floor(Math, random() * topics.length)];
+var randomTopic = topics[Math. floor(Math. random() * topics.length)];
 
 function listTopics() {
     for(var x = 0; x< topics.length; x++) {
